ddeccde677dcb5d99904bf9e8a7a98c5a550c088
Muhammad Zamroni <halo@matriphe.com>
2020-05-19 14:49:02 +0200

    Updat randomizer test

--- randomizer_test.go
+++ randomizer_test.go
@@ -11,8 +11,8 @@ func TestRandom_GetRandom(t *testing.T) {
 	randomizer := newRandomizer()
 	for i := 1; i <= 5; i++ {
 		t.Run(fmt.Sprintf("test_%d", i), func(t *testing.T) {
-			actual := randomizer.GetRandom(5)
-			assert.True(t, 0 <= actual && actual <= 5)
+			actual := randomizer.GetRandom(3)
+			assert.True(t, 0 <= actual && actual <= 3)
 		})
 	}
 }
